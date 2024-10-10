09b7ac5a377b2e821b719735936c630083931fbe
sushmitsarmah <sushmitsarmah@gmail.com>
2024-10-10 10:13:53 +0530

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,8 +5,7 @@ sudo apt-get update
 sudo apt install -y musl-tools bubblewrap gcc g++ gcc-multilib libclang-dev clang protobuf-compiler make cmake libssl-dev libseccomp-dev pkg-config
 
 # Install Rust SGX tools
-cargo install fortanix-sgx-tools
-cargo install sgxs-tools
+cargo install fortanix-sgx-tools sgxs-tools
 
 # Download and install Oasis CLI
 wget https://github.com/oasisprotocol/cli/releases/download/v0.10.1/oasis_cli_0.10.1_linux_amd64.tar.gz
